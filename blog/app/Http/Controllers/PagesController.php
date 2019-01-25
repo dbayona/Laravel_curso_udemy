508c0fd5d5aa3130f24db2bdeafcb9fa52fe7398
Daniel Bayona <dabayona@gmail.com>
2019-01-25 09:50:30 -0500

    Lección 10 del Curso de Laravel Udemy

--- blog/app/Http/Controllers/PagesController.php
+++ blog/app/Http/Controllers/PagesController.php
@@ -7,48 +7,55 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
 	protected $request;
-	
+
 	/*public function __construct(Request $request)
 	{
 		$this->request = $request;
 	}*/
-	
+
     //home
 	public function home()
 	{
 		return view('home');
 	}
-	
+
 	//contact
 	public function contact()
 	{
 		return view('contactos');
 	}
-	
+
 	//mensaje
 	public function mensajes(Request $request)
 	{
 		//return "Procesando el mensaje...";
 		//return $request->all();
-		
-		if($request->has('nombre'))
+
+		/*if($request->has('nombre'))
 		{
-			//return $request->all();
 			return "Si tiene nombre. Es " . $request->input('nombre');
 		}
-		
-		return "No tiene nombre";
+
+		return "No tiene nombre";*/
+
+		$this->validate($request, [
+															  'nombre' => 'required',
+																'email' => 'required|email',
+																'mensaje' => 'required|min:5'
+															]);
+
+		return $request->all();
 	}
-	
+
 	//saludo
 	public function saludo($nombre = "Invitado")
 	{
 		$html = "<h2>Contenido html </h2>"; //ingresado por formulario
-	
+
 		$script = "<script>alert('Problema XSS - Cross Site Scripting!')</script>"; //ingresado por formulario
-		
+
 		$consolas = ["Play station 4", "Xbox One", "Wii U"];
-		
+
 		return view('saludo', compact('nombre', 'html', 'script', 'consolas'));
 	}
 }
--- blog/resources/views/layout.blade.php
+++ blog/resources/views/layout.blade.php
@@ -7,6 +7,10 @@
 			text-decoration: none;
 			color: green;
 		}
+		.error{
+			color: red;
+			font-size: 12;
+		}
 	</style>
 	<title>Mi sitio</title>
 </head>
@@ -17,24 +21,24 @@
 		{
 			return request()->is($url) ? 'active' : '';
 		}?>
-		
+
 		<h1> {{request()->is('/') ? 'Estás en el home' : 'No estás en el home'}} </h1>
 		<nav>
 			<!--//=============== SIN BLADE =================
 			<a href="<?php echo route('home') ?>">Inicio</a>
 			<a href="<?php echo route('saludos', 'Daniel') ?>">Saludo</a>
 			<a href="<?php echo route('contactos') ?>">Contactos</a>-->
-			
+
 			<!--//=============== CON BLADE =================-->
 			<a class="{{activeMenu('/')}}" href="{{route('home')}}">Inicio</a>
 			<a class="{{activeMenu('saludos/*')}}" href="{{route('saludos', 'Daniel')}}">Saludo</a>
 			<a class="{{activeMenu('contactame')}}" href="{{route('contactos')}}">Contactos</a>
 		</nav>
 	</header>
-	
+
 	@yield('contenido')
-	
+
 	<footer>Copyright {{date('Y')}}</footer>
-	
+
 </body>
 </html>
